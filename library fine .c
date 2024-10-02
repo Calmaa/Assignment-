aa10761b3182b822de5021424d4e3e8c2730cf4c
Calmaa <calebmwendagitonga@gmail.com>
2024-10-02 16:04:09 +0300

    Update library fine .c

--- library fine .c
+++ library fine .c
@@ -8,31 +8,37 @@ date:26/9/2024
 int main(){
 int Book_id;
 int due_date;
-printf ("Enter Book_id");
-scanf ("%d",&Book_id);
-printf("Enter due_date");
-scanf("%d",&due_date);
+int fine_rate;
+int fine_amount;
+int days_overdue;
 int return_date;
-printf("Enter return_date");
+printf ("Enter Book_id:");
+scanf ("%d",&Book_id);
+printf("Enter due_date:");
+scanf("%d",&due_date);
+printf("Enter return_date:");
 scanf("%d", &return_date);
-int days_overdue=return_date-due_date;
-if(days_overdue<=7){
-int fine_rate=20;
-int fine_amount=fine_rate*days_overdue;
-    printf("Your fine is:%2d"),fine_amount);
+ days_overdue=return_date-due_date; 
+if (days_overdue<=0){
+  printf("The book is not overdue.\n"); 
+  printf("Your fine is 0\n");}
+else if (days_overdue<=7){
+fine_rate=20;
+fine_amount=fine_rate*days_overdue;
+    printf("Your fine is:%d\n",fine_amount);
     }
-    else if(days_overdue<=15)
+     else if (days_overdue<15)
     {
-    int fine_rate=50;
-   int fine_amount=fine_rate*days_overdue;
-    printf("Your fine is:%2d",fine_amount);
+    fine_rate=50;
+   fine_amount=fine_rate*days_overdue;
+    printf("Your fine is:%d\n",fine_amount);
     }
-    else (days_overdue>=15)
-    {
-    int fine rate =100;
-    int fine_amount=fine_rate*days_overdue;
+    else{
     
-    printf("Your fine is %2d",fine amount);
+    fine_rate =100;
+    fine_amount=fine_rate*days_overdue;
+    
+    printf("Your fine is %d\n",fine_amount);
     }
 return 0 ;
 }
